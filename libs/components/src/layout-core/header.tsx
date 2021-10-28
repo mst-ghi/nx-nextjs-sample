bb5457a028347311f67abd03985a48cad72d612a
Mostafa Gholami <mostafagholamidev@gmail.com>
2021-10-28 17:42:15 +0330

    module federation ready for test

--- libs/components/src/layout-core/header.tsx
+++ libs/components/src/layout-core/header.tsx
@@ -5,24 +5,9 @@ export function Header(props: HeaderProps) {
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <a
-                  href="/"
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Micro1
-                </a>
-                <a
-                  href="/micro2"
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Micro2
-                </a>
-              </div>
-            </div>
-          </div>
+          <span className="text-white font-bold">
+            Sample Nx MicroFrontend [Next.js]
+          </span>
         </div>
       </div>
     </nav>
